9a2c1bc3284a5a1fbd7cd2caf0b6009f598da5f7
Eddy Martin <eddypmartin@gmail.com>
2022-07-05 17:05:43 -0400

    s8-c1 début navigation du carrousel avec radio

--- carrousel.php
+++ carrousel.php
@@ -1,38 +1,41 @@
 <?php 
 /**
- * 
- * Description: Affiche dans une boîte modale le contenu d'une galerie d'image en animant le passage d'une image à l'autre
+ * Plugin name: Carrousel
+ * Description: Carrousel d'images créé à partir d'une galerie d'images
  * Author: Eddy Martin
  * Plugin URI: https://github.com/eddytuto
  */
+function carrousel_31w_enqueue()
+{
+    $version_css = filemtime(plugin_dir_path(__FILE__) . "style.css");
+    $version_js = filemtime(plugin_dir_path(__FILE__) . "script/carrousel.js");
+    //var_dump(__FILE__); die()
+    wp_enqueue_style(   'carrousel_31w_css', 
+                        plugin_dir_url( __FILE__)  . "style.css",
+                        array(),
+                        $version_css);
 
-    function carrousel_enqueue(){
-        $version_js = filemtime(plugin_dir_path(__FILE__) . "script/carrousel.js");
-        // var_dump($version_script); die();
-        $version_css = filemtime(plugin_dir_path(__FILE__) . "style.css");
+    wp_enqueue_script(  'carrousel_31w_js', 
+                        plugin_dir_url( __FILE__) . "script/carrousel.js",
+                        array(),
+                        $version_js,
+                        true);
+}
+add_action('wp_enqueue_scripts', 'carrousel_31w_enqueue');
 
-        wp_enqueue_style(   "carrou",
-                            plugin_dir_url(__FILE__) . "style.css",
-                            array(),
-                            $version_css);
+function genere_carrousel(){
+    /////////////////////////////////////// HTML
+    // Le conteneur du carrousel
+    $contenu = '<div class="carrousel">';
+    // Le bouton fermeture
+    $contenu .= '<button class="carrousel__fermeture">X</button>';
+    // Le conteneur d'images
+    $contenu .= '<figure class="carrousel__figure"></figure>';
+    // Le formulaire des radio bouton pour naviguer d'une image à l'autre
+    $contenu .= '<form class="carrousel__radio"></form>';
+    $contenu .= '</div> <!-- fin du carrousel -->';
+   
+    return $contenu;
+}
 
-        wp_enqueue_script( "carrou",
-                            plugin_dir_url(__FILE__) . "script/carrousel.js",
-                            array(),
-                            $version_js,
-                            true);
-    }
-        
-    add_action('wp_enqueue_scripts', 'carrousel_enqueue');     
-
-
-    function carrousel_initialise(){
-        $contenu = "<div class='carrousel'>";
-        $contenu .= "<button class='carroussel__fermeture'>X</button>";
-        $contenu .= "<figure class='carrousel__img'></figure>";
-        $contenu .= "</div>";
-
-        return $contenu;
-    }
-
-    add_shortcode('carrousel', 'carrousel_initialise');
+add_shortcode('carrousel', 'genere_carrousel');
--- script/carrousel.js
+++ script/carrousel.js
@@ -1,16 +1,46 @@
 (function(){
     console.log('Début du carrousel')
+   
     let elmGalerie = document.querySelector('.galerie')
     let elmGalerieImg = elmGalerie.querySelectorAll("figure figure img")
     let elmCarrousel = document.querySelector('.carrousel')
     let elmCarrousel__figure = document.querySelector('.carrousel__figure')
+    let elmCarrousel__fermeture = document.querySelector('.carrousel__fermeture')
+    let elmCarrousel__radio = document.querySelector('.carrousel__radio')
     console.log(elmGalerieImg[0].getAttribute('src'))
+    let index = 0
     for (const img of elmGalerieImg){
+        ajouter_elmImg(img)
+        ajouter_elmRadio()
         img.addEventListener('mousedown', function(){
-            console.log(this.getAttribute('src'))
-            elmCarrousel.classList.add('carrousel__active')
-            console.log(elmCarrousel.classList)
-        })
-
+        elmCarrousel.classList.add('carrousel__ouvrir') 
+    }) 
     }
+//////////////////////////////////////////////////////////  ajouter_elmimg
+    function ajouter_elmImg(img){
+        let elmImg = document.createElement('img')
+        elmImg.classList.add('carrousel__figure__img')
+        elmImg.setAttribute('src', img.getAttribute('src'))
+        elmCarrousel__figure.appendChild(elmImg)
+        elmImg.dataset.index = index
+    }
+
+    function ajouter_elmRadio(){
+        let elmRadio = document.createElement('input')
+        elmRadio.setAttribute('type','radio')
+        elmRadio.setAttribute('name','radImg')
+        elmRadio.dataset.index = index
+        index = index+1
+        elmCarrousel__radio.appendChild(elmRadio)
+        elmRadio.addEventListener('mousedown', function(){
+            
+            elmCarrousel__figure.children[this.dataset.index].classList.add('carrousel__figure__img--activer')
+        })
+    }
+  
+
+
+    elmCarrousel__fermeture.addEventListener('mousedown', function(){
+        elmCarrousel.classList.remove('carrousel__ouvrir') 
+    })
 })()
